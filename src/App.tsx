fe8b5bb4937ec4f87f94627241d7b470716db98b
R3call <thenoobyzombie@gmail.com>
2023-12-03 20:54:16 +0100

    repushed

--- src/App.tsx
+++ src/App.tsx
@@ -15,22 +15,22 @@ const valves: ValveCollection = [
 ];
 
 function ValveComponent({ deviceId, status }: Valve) {
-  const mutation = useMutation({
-    mutationFn: (status: ValveState) => {
-      return changeValveState(deviceId, status);
-    }
-  });
+	const mutation = useMutation({
+		mutationFn: (status: ValveState) => {
+			return changeValveState(deviceId, status);
+		},
+	});
 
-  const onSubmit = (newStatus: ValveState) => {
-    mutation.mutate(newStatus);
-  };
-  
+	const onSubmit = (newStatus: ValveState) => {
+		mutation.mutate(newStatus);
+	};
+
 	return (
 		<div className="flex flex-row justify-center items-center gap-4 p-4">
 			<h1>{deviceId}</h1>
 			<p>Current Status: {status}</p>
-			<Button onClick={() => onSubmit('on')}>Turn on</Button>
-			<Button onClick={() => onSubmit('off')}>Turn off</Button>
+			<Button onClick={() => onSubmit("on")}>Turn on</Button>
+			<Button onClick={() => onSubmit("off")}>Turn off</Button>
 		</div>
 	);
 }
