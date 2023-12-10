eea0e78f5dbecadce95dcb0f30b22d74238e92df
R3call <thenoobyzombie@gmail.com>
2023-12-10 11:25:24 +0100

    updated: removed unused ui elements

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Fragment } from "react";
 import { Disclosure, Menu, Transition } from "@headlessui/react";
-import { AlignJustify, X, Droplet } from "lucide-react";
-
-const navigation = [
-    { name: "Dashboard", href: "/", current: true }
-];
+import { Droplet, CircleUserRound } from "lucide-react";
 
 function classNames(...classes: string[]) {
     return classes.filter(Boolean).join(" ");
@@ -17,43 +14,9 @@ export default function Navbar() {
             <>
                 <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
                 <div className="relative flex h-16 items-center justify-between">
-                    <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                    {/* Mobile menu button*/}
-                    <Disclosure.Button className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                        <span className="absolute -inset-0.5" />
-                        <span className="sr-only">Open main menu</span>
-                        {open ? (
-                        <X className="block h-6 w-6" aria-hidden="true" />
-                        ) : (
-                        <AlignJustify
-                            className="block h-6 w-6"
-                            aria-hidden="true"
-                        />
-                        )}
-                    </Disclosure.Button>
-                    </div>
                     <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
                     <div className="flex flex-shrink-0 items-center">
                         <Droplet className="h-8 w-auto" aria-hidden="true" color="#00D3F7"/>
-                    </div>
-                    <div className="hidden sm:ml-6 sm:block">
-                        <div className="flex space-x-4">
-                        {navigation.map((item) => (
-                            <a
-                            key={item.name}
-                            href={item.href}
-                            className={classNames(
-                                item.current
-                                ? "bg-gray-900 text-white"
-                                : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                                "rounded-md px-3 py-2 text-sm font-medium"
-                            )}
-                            aria-current={item.current ? "page" : undefined}
-                            >
-                            {item.name}
-                            </a>
-                        ))}
-                        </div>
                     </div>
                     </div>
                     <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
@@ -64,11 +27,10 @@ export default function Navbar() {
                         <Menu.Button className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                             <span className="absolute -inset-1.5" />
                             <span className="sr-only">Open user menu</span>
-                            <img
-                            className="h-8 w-8 rounded-full"
-                            src="https://cdn.freebiesupply.com/logos/large/2x/react-1-logo-png-transparent.png"
-                            alt=""
-                            />
+                            <a href="/">
+                                <CircleUserRound color="#00D3F7" className="h-8 w-8"/>
+                            </a>
+                            
                         </Menu.Button>
                         </div>
                         <Transition
@@ -100,27 +62,6 @@ export default function Navbar() {
                     </div>
                 </div>
                 </div>
-
-                <Disclosure.Panel className="sm:hidden">
-                <div className="space-y-1 px-2 pb-3 pt-2">
-                    {navigation.map((item) => (
-                    <Disclosure.Button
-                        key={item.name}
-                        as="a"
-                        href={item.href}
-                        className={classNames(
-                        item.current
-                            ? "bg-gray-900 text-white"
-                            : "text-gray-300 hover:bg-gray-700 hover:text-white",
-                        "block rounded-md px-3 py-2 text-base font-medium"
-                        )}
-                        aria-current={item.current ? "page" : undefined}
-                    >
-                        {item.name}
-                    </Disclosure.Button>
-                    ))}
-                </div>
-                </Disclosure.Panel>
             </>
             )}
         </Disclosure>
